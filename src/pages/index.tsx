aa7aaf25227b8d5b8b69cd2d1851331923558918
Terry Blanc <teblanc2@gmail.com>
2024-08-08 09:04:17 -0400

    - added url submit html

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,12 +20,22 @@ export default function Home() {
             Zek <span className="text-[hsl(280,100%,70%)]">Security</span> 
           </h1>
           <p className="text-sm font-extrabold tracking-tight text-white">Your 24/7 security monitoring solution powered by Googe Gemini AI</p>
-          <div className="flex flex-col items-center gap-2">
+          {/* <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
               {hello.data ? hello.data.greeting : "Loading tRPC query..."}
             </p>
             <AuthShowcase />
-          </div>
+          </div> */}
+          
+
+<form className="max-w-sm mx-auto">
+  <div className="mb-5">
+    <label for="RTSPURL" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">RTSP URL</label>
+    <input type="RTSPURL" id="RTSPURL" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="rtsp://10.0.0.1" required />
+  </div>
+  <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Stream</button>
+</form>
+
         </div>
       </main>
     </>
